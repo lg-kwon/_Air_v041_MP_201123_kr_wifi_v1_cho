4e5999b777ccfdb16f561209ce263cd96d5d0a38
kwonilgun <kwonilgun@naver.com>
2024-04-12 15:12:20 +0900

    다시바꾼다

--- SysTick_Example/main.c
+++ SysTick_Example/main.c
@@ -12,7 +12,7 @@
 
 /* Includes ------------------------------------------------------------------*/
 
-//git 테스트를 해 본다.  다시한번 해본다. KWONILGUN
+//git 테스트를 해 본다.  다시바꾸어본다
 #include "main.h"`
 
 /** @addtogroup STM32F4xx_StdPeriph_Examples
